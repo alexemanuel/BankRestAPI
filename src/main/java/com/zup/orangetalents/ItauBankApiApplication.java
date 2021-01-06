64264474a17d207f79ec4b845580d654773cbcbe
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-06 09:47:53 -0300

    Initial commit

---
+++ src/main/java/com/zup/orangetalents/ItauBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.zup.orangetalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauBankApiApplication.class, args);
+	}
+
+}
