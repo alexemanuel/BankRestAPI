a24b13b412ffbefab51fa083d6d3e8309aa218dc
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-06 14:13:14 -0300

    Feat: Implement ErrorDetails class
    The ErrorDetails class is reponsable to represent a error instance in
    the api response.

---
+++ src/main/java/com/zup/orangetalents/exception/ErrorDetails.java
@@ -0,0 +1,40 @@
+package com.zup.orangetalents.exception;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+	
+	private String status;
+	private String title;
+	private String details;
+	private String fieldName;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fieldName == null) ? 0 : fieldName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ErrorDetails other = (ErrorDetails) obj;
+		if (fieldName == null) {
+			if (other.fieldName != null)
+				return false;
+		} else if (!fieldName.equals(other.fieldName))
+			return false;
+		return true;
+	}
+}
