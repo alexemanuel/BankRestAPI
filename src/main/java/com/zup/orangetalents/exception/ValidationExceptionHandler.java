8a37d8ad4922e782e5960fbaaffbc3e68cc885ef
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-06 14:16:34 -0300

    Feat: method handlerMethodArgumentNotValidException
    This method is reponsable for handle the exceptions thrown during the
    field validations, instead of the default spring implementation. Through
    this method we can return, for the user, a set of structured errors
    objects.

---
+++ src/main/java/com/zup/orangetalents/exception/ValidationExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.zup.orangetalents.exception;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice 
+public class ValidationExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handlerMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		List<FieldError> errors = exception.getBindingResult().getFieldErrors();
+		Set<ErrorDetails> x = new HashSet<ErrorDetails>();
+		
+		for(FieldError error: errors) {
+			String details = error.getDefaultMessage();
+			x.add(new ErrorDetails(HttpStatus.BAD_REQUEST.toString(), 
+									"Validation Error", 
+									String.format(details, error.getRejectedValue()), 
+									error.getField()));
+		}	
+		return new ResponseEntity<>(x, HttpStatus.BAD_REQUEST);
+	}
+
+}
