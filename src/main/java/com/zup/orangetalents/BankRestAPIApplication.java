1d8f5e6fde02869e31556c0a4e9add1412b9b6fe
Alex Emanuel <alexprojetos27@gmail.com>
2021-01-07 18:10:59 -0300

    Refactor: Change project's name

--- src/main/java/com/zup/orangetalents/BankRestAPIApplication.java
+++ src/main/java/com/zup/orangetalents/BankRestAPIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItauBankApiApplication {
+public class BankRestAPIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ItauBankApiApplication.class, args);
+		SpringApplication.run(BankRestAPIApplication.class, args);
 	}
 
 }
